61792e4ab82a29d88bc0a130684ed27b84725969
codeknight1119 <code.knight1119@gmail.com>
2026-07-27 17:07:27 +0000

    test change

--- BP/scripts/main.js
+++
@@ -1,7 +0,0 @@
-import { system } from "@minecraft/server";
-
-const TEST_DIM_ID = "pulse:test";
-
-system.beforeEvents.startup.subscribe((event) => {
-  event.dimensionRegistry.registerCustomDimension(TEST_DIM_ID);
-});
