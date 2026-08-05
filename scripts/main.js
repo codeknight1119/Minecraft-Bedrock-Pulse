4412694a941680e53c537f8ab7f274868fe01ee8
codeknight1119 <code.knight1119@gmail.com>
2026-08-05 12:13:52 -0400

    make spawn platform in dim

--- scripts/main.js
+++ scripts/main.js
@@ -6,3 +6,11 @@ const DIMENSION_ID = "custom:my_dimension";
 system.beforeEvents.startup.subscribe((event) => {
     event.dimensionRegistry.registerCustomDimension(DIMENSION_ID);
 });
+
+world.afterEvents.worldInitialize.subscribe(() => {
+    // Get reference to your custom dimension
+    const customDim = world.getDimension(DIMENSION_ID);
+
+    // Build a 5x5 stone platform around (0, 100, 0) directly from the server
+    customDim.runCommand("fill -2 100 -2 2 100 2 stone");
+});
